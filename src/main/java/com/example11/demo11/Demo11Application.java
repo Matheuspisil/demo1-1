40e0e234caa278773cddfec555b18ba2ce183edd
Matheus Pires e Silva <67922748+Matheuspisil@users.noreply.github.com>
2022-05-10 21:36:27 -0300

    add coment, start project

--- src/main/java/com/example11/demo11/Demo11Application.java
+++ src/main/java/com/example11/demo11/Demo11Application.java
@@ -8,6 +8,7 @@ public class Demo11Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo11Application.class, args);
+		//iniciando dia 10/05/22 prazo indeterminado
 	}
 
 }
