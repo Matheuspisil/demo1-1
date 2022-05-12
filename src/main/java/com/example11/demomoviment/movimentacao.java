146312977fe7516c7178e573d001e1f7da0627e3
Matheus Pires e Silva <67922748+Matheuspisil@users.noreply.github.com>
2022-05-12 19:53:18 -0300

    atributos da class movimentacao e interação com enum movimentacaoTipo

--- src/main/java/com/example11/demomoviment/movimentacao.java
+++ src/main/java/com/example11/demomoviment/movimentacao.java
@@ -1,5 +1,42 @@
 package com.example11.demomoviment;
 
-public class movimentacao {
+import java.time.LocalDateTime;
 
+public class movimentacao {
+	private Integer id;
+	private LocalDateTime dataHora;
+	private String descricao;
+	private Double valor;
+	private movimentacaoTipo tipo;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	public movimentacaoTipo getTipo() {
+		return tipo;
+	}
+	public void setTipo(movimentacaoTipo tipo) {
+		this.tipo = tipo;
+	}
+	
 }
